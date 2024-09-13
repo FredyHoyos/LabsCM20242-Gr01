ad545d14f47c525198e33a69de2a6f8e78109688
FredyHoyos <fredy.hoyos1@udea.edu.co>
2024-09-12 20:37:19 -0500

    Se mejora las entradas de texto (apariencia) se agrega una nueva entrada de texto y se coloca un boton

--- Lab1-UI/src/main/java/co/edu/udea/compumovil/gr01_20242/lab1/MainActivity.kt
+++ Lab1-UI/src/main/java/co/edu/udea/compumovil/gr01_20242/lab1/MainActivity.kt
@@ -1,43 +1,53 @@
 package co.edu.udea.compumovil.gr01_20242.lab1
 
 import android.os.Bundle
+import android.text.style.BackgroundColorSpan
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.DatePicker
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Popup
@@ -59,67 +69,106 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Titulo(modifier: Modifier = Modifier) {
-    Text(
-        text = "Información Personal",
-        modifier = modifier,
-        textAlign = TextAlign.Center
+fun Titulo() {
+    TopAppBar(
+        title = { Text("Información Personal") },
+        Modifier.background(Color.Red, RectangleShape)
     )
+
 }
 
+
+
 @Composable
-fun Cuerpo(modifier: Modifier = Modifier) {
+fun Cuerpo() {
+    var nombre by remember { mutableStateOf("") }
+    var apellido by remember { mutableStateOf("") }
+
 
     Column (
         modifier = Modifier.padding(10.dp).fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Row{
+        Row(
+            modifier = Modifier.fillMaxWidth(), // que la fila ocupe todo el ancho disponible
+            verticalAlignment = Alignment.CenterVertically // Alinea los elementos verticalmente en el centro
+        ){
             Image(painter = painterResource(id = R.drawable.ic_launcher_background),
                 contentDescription = "Imagen nombre",
                 modifier = Modifier.size(70.dp).padding(vertical = 8.dp))
-            EntradaDeTexto("nombre")
+            OutlinedTextField(
+                value = nombre,
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
+                onValueChange = {
+                    if(it.length < 50)
+                        nombre = it
+                },
+                label = { Text("Escriba su nombre") },
+            )
         }
-        Row{
+        Row(
+            modifier = Modifier.fillMaxWidth(), // que la fila ocupe todo el ancho disponible
+            verticalAlignment = Alignment.CenterVertically // Alinea los elementos verticalmente en el centro
+        ){
             Image(painter = painterResource(R.drawable.ic_launcher_background),
                 contentDescription = "Imagen apellido",
                 modifier = Modifier.size(70.dp).padding(vertical = 8.dp))
-            EntradaDeTexto("apellido")
-
+            OutlinedTextField(
+                value = apellido,
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
+                onValueChange = {
+                    if(it.length < 50)
+                        apellido = it
+                },
+                label = { Text("Escriba su apellido") },
+            )
         }
-        Row{
-            Image(painter = painterResource(R.drawable.ic_launcher_background),
+        Row(
+            modifier = Modifier.fillMaxWidth(), //que la fila ocupe todo el ancho disponible
+            verticalAlignment = Alignment.CenterVertically // Alinea los elementos verticalmente en el centro
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.ic_launcher_background),
                 contentDescription = "Imagen sexo",
-                modifier = Modifier.size(70.dp).padding(vertical = 8.dp))
+                modifier = Modifier.size(70.dp).padding(vertical = 8.dp)
+            )
+            Spacer(modifier = Modifier.width(8.dp)) // Añade un espacio entre la imagen y el texto
             Text("Sexo: ")
-
         }
-        Row{
+        Row(
+            modifier = Modifier.fillMaxWidth(), //que la fila ocupe todo el ancho disponible
+            verticalAlignment = Alignment.CenterVertically // Alinea los elementos verticalmente en el centro
+        ){
             Image(painter = painterResource(R.drawable.ic_launcher_background),
-                contentDescription = "Imagen sexo",
+                contentDescription = "Imagen de nacimiento",
                 modifier = Modifier.size(70.dp).padding(vertical = 8.dp))
             Text("Fecha de nacimiento: ")
             DatePickerDocked()
 
         }
+        Row(
+            modifier = Modifier.fillMaxWidth(), //que la fila ocupe todo el ancho disponible
+            verticalAlignment = Alignment.CenterVertically // Alinea los elementos verticalmente en el centro
+        ){
+            Image(painter = painterResource(R.drawable.ic_launcher_background),
+                contentDescription = "Imagen escolaridad",
+                modifier = Modifier.size(70.dp).padding(vertical = 8.dp))
+            Text("Grado de Escolaridad ")
+
+        }
+
+        Button(
+            modifier = Modifier.background(Color.Green),
+            onClick = {}
+        ) {
+            Text("Siguiente")
+        }
     }
 }
 
 
-@Composable
-fun EntradaDeTexto(visual: String) {
-    val texto = remember { mutableStateOf("") }
-
-    TextField(
-        value = texto.value,
-        onValueChange = { newText ->
-            texto.value = newText
-        },
-        label = { Text("Escriba su $visual") },
-        modifier = Modifier.padding(vertical = 8.dp)
-    )
-}
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
